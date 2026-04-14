8669ffe969bd2783e023caaf7e8c828d3d878e4e
konkek18 <ST10481607@rcconnect.edu.za>
2026-04-14 07:54:21 +0200

    Add files via upload
    Added chatbot logic

---
+++ Chatbot.cs
@@ -0,0 +1,28 @@
+﻿
+namespace CyberSecurityChatbot
+{
+    internal class Chatbot
+    {
+        public string GetResponse(string input)
+        {
+            input = input.ToLower();
+
+            if (input.Contains("how are you"))
+                return "I'm just a bot, but im here to help!";
+
+            if (input.Contains("purpose"))
+                return "My purpose isnto help you stay safe online.";
+
+            if (input.Contains("password"))
+                return "Use strong passwords with letters, numbers, and symbols.";
+
+            if (input.Contains("phishing"))
+                return "Phishing is a scam where attackers trick you into giving personal information.";
+
+            return "I don't understand that. Try asking about cybersecurity.";
+
+
+        }
+    }
+
+}
